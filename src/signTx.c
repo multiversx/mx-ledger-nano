518b1b7d78685212bd6d44cd17d6c8bde5e5e023
Glonk <mihaibarca79@yahoo.com>
2020-07-19 17:26:31 +0300

    added the fee on display for user confirmation when signing tx

--- src/signTx.c
+++ src/signTx.c
@@ -3,6 +3,7 @@
 #include "ux.h"
 #include "utils.h"
 #include <jsmn.h>
+#include <stdlib.h>
 
 typedef struct {
     char buffer[MAX_BUFFER_LEN]; // buffer to hold large transactions that are composed from multiple APDUs
@@ -10,6 +11,9 @@ typedef struct {
 
     char receiver[FULL_ADDRESS_LENGTH];
     char amount[MAX_AMOUNT_LEN];
+    unsigned long long gas_limit;
+    unsigned long long gas_price;
+    char fee[MAX_AMOUNT_LEN];
     char signature[64];
 } tx_context_t;
 
@@ -37,8 +41,15 @@ UX_STEP_NOCB(
       .title = "Amount",
       .text = tx_context.amount,
     });
+UX_STEP_NOCB(
+    ux_sign_tx_flow_10_step,
+    bnnn_paging,
+    {
+      .title = "Fee",
+      .text = tx_context.fee,
+    });
 UX_STEP_VALID(
-    ux_sign_tx_flow_10_step, 
+    ux_sign_tx_flow_11_step, 
     pb, 
     sendResponse(setResultSignature(), true),
     {
@@ -46,7 +57,7 @@ UX_STEP_VALID(
       "Sign transaction",
     });
 UX_STEP_VALID(
-    ux_sign_tx_flow_11_step, 
+    ux_sign_tx_flow_12_step, 
     pb,
     sendResponse(0, false),
     {
@@ -58,7 +69,8 @@ UX_FLOW(ux_sign_tx_flow,
   &ux_sign_tx_flow_8_step,
   &ux_sign_tx_flow_9_step,
   &ux_sign_tx_flow_10_step,
-  &ux_sign_tx_flow_11_step
+  &ux_sign_tx_flow_11_step,
+  &ux_sign_tx_flow_12_step
 );
 
 static uint8_t setResultSignature() {
@@ -68,6 +80,49 @@ static uint8_t setResultSignature() {
     os_memmove(G_io_apdu_buffer + tx, tx_context.signature, sig_size);
     tx += sig_size;
     return tx;
+}
+
+// convert a string to a unsinged long long
+unsigned long long char2ULL(char *str) {
+    unsigned long long result = 0; // Initialize result
+    // Iterate through all characters of input string and update result
+    for (int i = 0; str[i] != '\0'; ++i)
+        result = result*10 + str[i] - '0';
+    return result;
+}
+
+// make the ERD fee look pretty. Add decimals and decimal point
+void makeFeePretty(unsigned long long fee) {
+    int len = 0;
+    tx_context.fee[0] = '0';
+    tx_context.fee[1] = '\0';
+    while (fee > 0) {
+        char digit = fee % 10;
+        fee /= 10;
+        len++;
+        os_memmove(tx_context.fee + 1, tx_context.fee, len);
+        tx_context.fee[len] = '\0';
+        tx_context.fee[0] = digit + '0';
+    }
+
+    len = strlen(tx_context.fee);
+    int missing = DECIMAL_PLACES - len + 1;
+    if (missing > 0) {
+        os_memmove(tx_context.fee + missing, tx_context.fee, len + 1);
+        os_memset(tx_context.fee, '0', missing);
+    }
+    len = strlen(tx_context.fee);
+    int dotPos = len - DECIMAL_PLACES;
+    os_memmove(tx_context.fee + dotPos + 1, tx_context.fee + dotPos, DECIMAL_PLACES + 1);
+    tx_context.fee[dotPos] = '.';
+    while (tx_context.fee[strlen(tx_context.fee) - 1] == '0') {
+        tx_context.fee[strlen(tx_context.fee) - 1] = '\0';
+    }
+    if (tx_context.fee[strlen(tx_context.fee) - 1] == '.') {
+        tx_context.amount[strlen(tx_context.fee) - 1] = '\0';
+    }
+    char suffix[5] = " ERD\0";
+    os_memmove(tx_context.fee + strlen(tx_context.fee), suffix, 5);
 }
 
 // make the ERD amount look pretty. Add decimals and decimal point
@@ -114,14 +169,12 @@ uint16_t txDataReceived(uint8_t *dataBuffer, uint16_t dataLength) {
 // parseData parses the received tx data
 uint16_t parseData() {
     int i, r;
-
     jsmn_parser p;
     jsmntok_t t[20]; // We expect no more than 20 tokens
 
     jsmn_init(&p);
     r = jsmn_parse(&p, tx_context.buffer, tx_context.bufLen, t, sizeof(t)/sizeof(t[0]));
     if (r < 1 || t[0].type != JSMN_OBJECT) {
-        // unable to parse. maybe we did not receive the entire tx so we return 'ok'
         return ERR_MESSAGE_INCOMPLETE;
     }
     int found_args = 0;
@@ -144,20 +197,40 @@ uint16_t parseData() {
             makeAmountPretty();
             found_args++;
         }
+        if (jsoneq(tx_context.buffer, &t[i], "gasLimit") == 0) {
+            if (len >= MAX_AMOUNT_LEN)
+                return ERR_AMOUNT_TOO_LONG;
+            char gas_limit[MAX_AMOUNT_LEN];
+            os_memmove(gas_limit, tx_context.buffer + t[i + 1].start, len);
+            gas_limit[len] = '\0';
+            tx_context.gas_limit = char2ULL(gas_limit);
+            found_args++;
+        }
+        if (jsoneq(tx_context.buffer, &t[i], "gasPrice") == 0) {
+            if (len >= MAX_AMOUNT_LEN)
+                return ERR_AMOUNT_TOO_LONG;
+            char gas_price[MAX_AMOUNT_LEN];
+            os_memmove(gas_price, tx_context.buffer + t[i + 1].start, len);
+            gas_price[len] = '\0';
+            tx_context.gas_price = char2ULL(gas_price);
+            found_args++;
+        }
 		if (jsoneq(tx_context.buffer, &t[i], "data") == 0) {
             hasDataField = true;
         }
     }
-    // found_args should be 2 if we identified both receiver and amount
-    if (found_args != 2) 
+    // found_args should be 4 if we identified the receiver, amount, gasLimit and gasPrice
+    if (found_args != 4) 
         return ERR_INVALID_MESSAGE;
     // check if the data field is not empty and contract data is not enabled from the menu
     if (hasDataField && N_storage.setting_contract_data == 0)
         return ERR_CONTRACT_DATA_DISABLED;
+    unsigned long long fee = tx_context.gas_limit * tx_context.gas_price;
+    makeFeePretty(fee);
     return MSG_OK;
 }
 
-// signTx performs the actual tx signing after the full tx data has beed received
+// signTx performs the actual tx signing after the full tx data has been received
 void signTx() {
     cx_ecfp_private_key_t privateKey;
     BEGIN_TRY {
