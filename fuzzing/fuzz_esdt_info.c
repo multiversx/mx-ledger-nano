f018ea8deb323cdea979963be3fdf6c5afcef10b
greenknot <greenknot@users.noreply.github.com>
2021-11-29 14:59:51 +0100

    pass esdt_info as a parameter to handle_provide_ESDT_info()
    It allows ASAN to detect overflows.

--- fuzzing/fuzz_esdt_info.c
+++ fuzzing/fuzz_esdt_info.c
@@ -2,12 +2,18 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "provide_ESDT_info.h"
 
-esdt_info_t esdt_info;
+static esdt_info_t *esdt_info_heap = NULL;
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  handle_provide_ESDT_info(data, size);
+  if (esdt_info_heap == NULL) {
+    esdt_info_heap = malloc(sizeof(*esdt_info_heap));
+  }
+
+  handle_provide_ESDT_info(data, size, esdt_info_heap);
+
   return 0;
 }
