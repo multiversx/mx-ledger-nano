cbfc33814a6bc26659c5ec52b9e423816fc25b75
Bogdan Rosianu <bogdan.rosianu@yahoo.com>
2023-07-20 14:01:34 +0300

    further updates

--- src/get_private_key.c
+++ src/get_private_key.c
@@ -14,7 +14,7 @@ static const uint32_t derive_path[BIP32_PATH] = {44 | HARDENED_OFFSET,
 bool get_private_key(uint32_t account_index,
                      uint32_t address_index,
                      cx_ecfp_private_key_t *private_key) {
-    uint8_t private_key_data[32];
+    uint8_t private_key_data[64];
     uint32_t bip32_path[BIP32_PATH];
     bool success = true;
 
@@ -23,18 +23,22 @@ bool get_private_key(uint32_t account_index,
     bip32_path[2] = account_index | HARDENED_OFFSET;
     bip32_path[4] = address_index | HARDENED_OFFSET;
 
-    os_derive_bip32_with_seed_no_throw(HDW_ED25519_SLIP10,
-                                       CX_CURVE_Ed25519,
-                                       bip32_path,
-                                       BIP32_PATH,
-                                       private_key_data,
-                                       NULL,
-                                       NULL,
-                                       0);
-    int ret_code =
-        cx_ecfp_init_private_key_no_throw(CX_CURVE_Ed25519, private_key_data, 32, private_key);
+    int ret_code = os_derive_bip32_with_seed_no_throw(HDW_ED25519_SLIP10,
+                                                      CX_CURVE_Ed25519,
+                                                      bip32_path,
+                                                      BIP32_PATH,
+                                                      private_key_data,
+                                                      NULL,
+                                                      NULL,
+                                                      0);
     if (ret_code != 0) {
         success = false;
+    } else {
+        ret_code =
+            cx_ecfp_init_private_key_no_throw(CX_CURVE_Ed25519, private_key_data, 32, private_key);
+        if (ret_code != 0) {
+            success = false;
+        }
     }
     explicit_bzero(private_key_data, sizeof(private_key_data));
 
